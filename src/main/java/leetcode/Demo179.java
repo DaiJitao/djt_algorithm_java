cdab1a64d25af20812f736d338138d313296d5d3
daijita <976185561@qq.com>
2022-09-02 11:58:19 +0800

    add code

--- src/main/java/leetcode/Demo179.java
+++ src/main/java/leetcode/Demo179.java
@@ -1,13 +1,36 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class Demo179 {
 
-    public void demo179(int []nums){
-        int n = nums.length;
-        Integer[] mynums = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            mynums[i] = nums[i];
-        }
+    public static void main(String[] args) {
+        int[] nums = {12, 13};
+        Demo179.demo179(nums);
+    }
 
+    public static void demo179(int []nums){
+        int n = nums.length;
+        String[] mynums = new String[n];
+        for (int i = 0; i < n; i++) {
+            mynums[i] = "" + nums[i];
+        }
+        Arrays.sort(mynums, Demo179::compare);
+
+        for (int i = 0; i < mynums.length; i++) {
+            System.out.print(mynums[i] + ' ');
+        }
+    }
+
+    private static int compare(String a, String b) {
+        String sa = a + b;
+        String sb = b + a;
+        return sb.compareTo(sa);
+    }
+
+    private static int compare(String a, String b, String c) {
+        String sa = a + b;
+        String sb = b + a;
+        return sb.compareTo(sa);
     }
 }
