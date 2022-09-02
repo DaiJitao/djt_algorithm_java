eb807e3ad2e24eedd7843023b4cca07f808494ab
daijita <976185561@qq.com>
2022-09-02 11:14:01 +0800

    del spark and kafka code

---
+++ src/main/java/leetcode/Demo179.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Demo179 {
+
+    public void demo179(int []nums){
+        int n = nums.length;
+        Integer[] mynums = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            mynums[i] = nums[i];
+        }
+
+    }
+}
